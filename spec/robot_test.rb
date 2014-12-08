3f54735bb30cb92ded2a36492d9cb39a5fa58d17
Mark Hollands <markh@lifx.co>
2014-12-08 11:47:28 +0000

    Adding more movement tests

--- spec/robot_test.rb
+++ spec/robot_test.rb
@@ -1,14 +1,17 @@
 require_relative '../robot'
 
 describe "tests the different operations a robot can do" do
+
+	let(:robot) do
+		Robot.new
+	end
+
 	it "robot faces east" do
-		robot = Robot.new
 		robot.face(:east)
 		expect(robot.orientation).to eq(:east)
 	end
 
 	it "robot turns left" do
-		robot = Robot.new
 		robot.face(:north)
 		robot.left
 
@@ -16,7 +19,6 @@ describe "tests the different operations a robot can do" do
 	end
 
 	it "robot turns right" do
-		robot = Robot.new
 		robot.face(:east)
 		robot.right
 
@@ -24,23 +26,34 @@ describe "tests the different operations a robot can do" do
 	end
 
 	it "robot tries to turn left without having been faced" do
-		robot = Robot.new
 		robot.right
 		expect(robot.orientation).to eq(nil)
 	end
 
 	it "robot is placed and reports" do
-		robot = Robot.new
 		robot.place(0,0,"EAST")
 		expect(robot.report).to eq("0,0,EAST")
 	end
 
 	it "robot is placed and reports and moves" do
-		robot = Robot.new
 		robot.place(0,0,"NORTH")
 		expect(robot.report).to eq("0,0,NORTH")
 		robot.move
 		expect(robot.report).to eq("0,1,NORTH")
-		
+	end
+
+	it "robots turns and then reports" do
+		robot.place(0,0,"NORTH")
+		robot.left
+		expect(robot.report).to eq("0,0,WEST")
+	end
+
+	it "robot moves and turns and moves" do
+		robot.place(1,2,"EAST")
+		robot.move
+		robot.move
+		robot.left
+		robot.move
+		expect(robot.report).to eq("3,3,NORTH")
 	end
 end
